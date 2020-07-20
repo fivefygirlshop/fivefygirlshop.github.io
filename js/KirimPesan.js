669b25a2aa52e9d4420f0791bc99fc3e44c387d1
fivefygirlshop <68543130+fivefygirlshop@users.noreply.github.com>
2020-07-20 19:24:00 +0700

    Update KirimPesan.js

--- js/KirimPesan.js
+++ js/KirimPesan.js
@@ -3,7 +3,7 @@ function SendWAMessage() {
     var xPesan = document.getElementById("yourpesan").value;
     var link = document.getElementById("mylink");
     var yourNumber = "6288220063867";
-    var yourMessage = 'Hallo, Nama saya ' + xNama +', '+ xPesan.split(' ').join('%20%20');
+    var yourMessage = 'Hallo, Nama saya ' + xNama +', '+ xPesan.split(' ').join('%20');
 
     var linkWhatsapp = 'https://api.whatsapp.com/send?phone=' + yourNumber + '&text=%20' + yourMessage;
 
