99050bc63e8d3894b9aef0c6789bce6fcd68dc1a
fivefygirlshop <68543130+fivefygirlshop@users.noreply.github.com>
2020-07-21 13:44:08 +0700

    Update KirimPesan.js

--- js/KirimPesan.js
+++ js/KirimPesan.js
@@ -48,3 +48,44 @@ function SendWAMessage() {
 
   return false;
 }
+
+function validateForm() {
+  var fNama = document.forms["myForm"]["yournama"].value;
+  var fHape = document.forms["myForm"]["yourhape"].value;
+  var fJalan = document.forms["myForm"]["yourjalan"].value;
+  var fRT = document.forms["myForm"]["yourrt"].value;
+  var fRW = document.forms["myForm"]["yourrw"].value;
+  var fKel = document.forms["myForm"]["yourkel"].value;
+  var fKec = document.forms["myForm"]["yourkec"].value;
+  var fKab = document.forms["myForm"]["yourkab"].value;
+  var fKodepos = document.forms["myForm"]["yourkodepos"].value;
+
+  if (fNama == "") {
+      alert("Nama anda belum diisi");
+      return false;
+  } else if (fHape == "") {
+      alert("No. Hape belum diisi");
+      return false;
+  } else if (fJalan == "") {
+      alert("Alamat Jalan belum diisi");
+      return false;
+  } else if (fRT == "") {
+      alert("RT belum diisi");
+      return false;
+  } else if (fRW == "") {
+      alert("RW belum diisi");
+      return false;
+  } else if (fKel == "") {
+      alert("Kelurahan belum diisi");
+      return false;
+  } else if (fKec == "") {
+      alert("Kecamatan belum diisi");
+      return false;
+  } else if (fKab == "") {
+      alert("Kabupaten/Kota belum diisi");
+      return false;
+  } else if (fKodepos == "") {
+      alert("Kode Pos belum diisi");
+      return false;
+  }
+}
